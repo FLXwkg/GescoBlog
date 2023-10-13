2818460c0332d146e8eb4652fd4bcd9e88b217f2
FLX <ewenheas13@gmail.com>
2023-10-13 08:49:20 +0000

    :art: : Merge Section et RouteCat dans CatRepo

--- src/CategoriesRepository.php
+++ src/CategoriesRepository.php
@@ -24,4 +24,37 @@ class CategoriesRepository{
 
         return $stmt->fetchAll(PDO::FETCH_CLASS, Categorie::class);
     }
+
+    public function getIdByName(string $routeCategory)
+    {
+        $pdo = $this->getPDO();
+        $sql = "SELECT id_categorie FROM categorie WHERE nom_categorie = :routeCategory;";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':routeCategory', $routeCategory, PDO::PARAM_STR);
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_CLASS, Categorie::class);
+    }
+
+    public function getIdByArticle(string $titreArticle)
+    {
+        $pdo = $this->getPDO();
+        $sql = "SELECT id_categorie FROM article WHERE titre_article = :titreArticle;";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':titreArticle', $titreArticle, PDO::PARAM_STR);
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_CLASS, Categorie::class);
+    }
+
+    public function getAll()
+    {
+        $pdo = $this->getPDO();
+        $sql = "SELECT * FROM categorie;";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_CLASS, Categorie::class);
+    }
+
 }
