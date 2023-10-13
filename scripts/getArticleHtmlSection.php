de54ec0d78aff0e6aba9b30eb5c9c927c45feb7b
FLX <ewenheas13@gmail.com>
2023-10-13 14:13:10 +0000

    :bug: : Rajout slugifyText

--- scripts/getArticleHtmlSection.php
+++ scripts/getArticleHtmlSection.php
@@ -7,12 +7,12 @@ function getArticleHtmlSection($maxLength, Categorie $category,Article $article)
 {
     $text = $article->getTexte();
     $href = strtolower(rtrim($category->getNom())) ?? 'non_defini';
-    $title = strtolower($article->getTitre()) ?? 'Titre';
+    $title = slugifyText($article->getTitre()) ?? 'Titre';
     $content =  '<span class="truncated-text">' . substr($text, 0, $maxLength) . '</span>';
     if (strlen($text) > $maxLength){ 
         return <<<HTML
             $content<br>
-            <a href='/$href/$title'>Voir plus</a>
+            <a href='/$href/$title' target="_blank" >Voir plus</a>
             HTML;
     }else{
         return <<<HTML
