e3e78b5d357643774a5f53f2e26f5ef0984fd3a1
Denis Métral <denisdeejay@gmail.com>
2023-10-24 15:28:36 +0200

    :art: route / et code cleanup

--- app/routes.php
+++ app/routes.php
@@ -11,33 +11,35 @@ use App\GenericController;
 use App\HomeController;
 use App\ArticleController;
 
-require __DIR__."/../scripts/unslugifyText.php";
+require __DIR__ . "/../scripts/unslugifyText.php";
 
 return function (App $app) {
-    $app->options('/{routes:.*}', function (Request $request, Response $response) {
-        // CORS Pre-Flight OPTIONS Request Handler
-        return $response;
+
+    $homeCategory = 14;
+
+
+    /**
+     * Page d'accueil
+     */
+    $app->get('/', function (Request $request, Response $response) use ($homeCategory) {
+        $homeController = new HomeController();
+        return $homeController->handleRoute($request, $response, ['page' => 'home'], $homeCategory);
     });
 
-    $app->get('/', function (Request $request, Response $response) {
-        $response->getBody()->write('Hello Blog!');
-        return $response->withHeader('Location', "/home")->withStatus(301);
-    });
 
-    //Routes des différentes catégories
-
+    /**
+     * Les catégories principales
+     */
     $app->get('/{page}', function ($request, $response, $args) {
         $route = new CategoriesRepository();
         $id = $route->getIdByName($args['page']);
-        if($args['page']=== 'home'){
-            $homeController = new HomeController();
-            return $homeController->handleRoute($request, $response, $args, $id[0]->getId());
-        }else{
-            $genericController = new GenericController();
-            return $genericController->handleRoute($request, $response, $args, $id[0]->getId());
+        if ($args['page'] === 'home') {
+            return $response->withHeader('Location', "/")->withStatus(301);
         }
+        $genericController = new GenericController();
+        return $genericController->handleRoute($request, $response, $args, $id[0]->getId());
     });
-    
+
     // Route dynamique pour afficher un article
 
     $app->get('/{page}/{titre_article}', function ($request, $response, $args) {
@@ -48,9 +50,9 @@ return function (App $app) {
         $Categorie = $route->getNameById($id[0]->getId());
         $nomCategorie = strtolower($Categorie[0]->getNom());
         //var_dump("/$nomCategorie/$titreArticleSlug");die();
-        if($args['page'] === 'home'){
+        if ($args['page'] === 'home') {
             return $response->withHeader('Location', "/$nomCategorie/$titreArticleSlug")->withStatus(301);
-        }else{
+        } else {
             $articleController = new ArticleController();
             return $articleController->render($request, $response, $args, $titreArticle, $id[0]->getId());
         }
