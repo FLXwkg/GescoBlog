706142f3f56302e3243da1b4fae09f5969a90dcf
FLX <ewenheas13@gmail.com>
2023-10-30 11:23:39 +0000

    :bento: :Layout v5.1.10: Liens header

--- templates/home.php
+++ templates/home.php
@@ -34,7 +34,7 @@ include "../scripts/slugifyText.php";
     </title>
 </head>
 <body>
-    <div class="blog-top">
+    <header class="blog-header">
         <div class="row container-fluid mx-0 px-0">
             <div class="blog-header-logo col-2 pt-4 px-4 pb-2">
                 <a href="/">
@@ -42,50 +42,51 @@ include "../scripts/slugifyText.php";
                 </a>
             </div>
             <div class="col-8 container mx-0">
-                <header class="row blog-header py-3">
-                    <div class="col-lg-0 col-xl-4 space-col"></div>
-                    <div class="col-lg-4 col-xl-4 blog-header-title text-center">
-                        <a href="/">
-                            <img src="/images/favicon.ico" alt="FLX Blog">
-                        </a>
-                    </div>
-                    <div class="col-lg-8 col-xl-4 search">
-                        <form class="row" role="search">
-                            <input class="col-9 search-input rounded" type="search" placeholder="Search" id="Search" aria-label="Search">
-                            <button class="col-3 btn btn-outline-secondary" type="submit">Search</button>
-                        </form>
-                    </div>
-                </header>
-                <div class="nav row">
-                    <nav class="navbar navbar-expand-lg py-1">
-                        <div class="container-fluid d-flex justify-content-evenly">
-                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" 
-                                    aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
-                            <div class="collapse navbar-collapse justify-content-evenly"  id="navbarTogglerDemo01">
-                                <?php 
-                                foreach ($sections as $section):
-                                    if (!$isHome && $section->getNom() === $category->getNom()):?>
-                                        <a class="actual-page-link nav-link p-2" href="/<?php echo $section->getSlug() ?? 'Non défini';?>">
-                                            <?php echo $section->getNom() ?? 'Non Défini'; ?>
-                                        </a>
-                                    <?php else : ?>
-                                        <a class="nav-link p-2" href="/<?php echo $section->getSlug() ?? 'Non défini';?>">
-                                            <?php echo $section->getNom() ?? 'Non Défini'; ?>
-                                        </a>
-                                    <?php
-                                    endif;
-                                endforeach ?>
-                            </div>
+                <div class="row py-3">
+                    <div class="row blog-top pb-2">
+                        <div class="col-lg-0 col-xl-4 space-col"></div>
+                        <div class="col-lg-4 col-xl-4 blog-header-title text-center">
+                            <a href="/">
+                                <img src="/images/favicon.ico" alt="FLX Blog">
+                            </a>
                         </div>
-                        
-                    </nav>
+                        <div class="col-lg-8 col-xl-4 search">
+                            <form class="row" role="search">
+                                <input class="col-9 search-input rounded" type="search" placeholder="Search" id="Search" aria-label="Search">
+                                <button class="col-3 btn btn-outline-secondary" type="submit">Search</button>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="nav row">
+                        <nav class="navbar navbar-expand-lg py-1">
+                            <div class="container-fluid d-flex justify-content-evenly">
+                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" 
+                                        aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>
+                                <div class="collapse navbar-collapse justify-content-evenly"  id="navbarTogglerDemo01">
+                                    <?php 
+                                    foreach ($sections as $section):
+                                        if (!$isHome && $section->getNom() === $category->getNom()):?>
+                                            <a class="actual-page-link nav-link p-2" href="/<?php echo $section->getSlug() ?? 'Non défini';?>">
+                                                <?php echo $section->getNom() ?? 'Non Défini'; ?>
+                                            </a>
+                                        <?php else : ?>
+                                            <a class="nav-link p-2" href="/<?php echo $section->getSlug() ?? 'Non défini';?>">
+                                                <?php echo $section->getNom() ?? 'Non Défini'; ?>
+                                            </a>
+                                        <?php
+                                        endif;
+                                    endforeach ?>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
                 </div>
             </div>
             <div class="col-2"></div>   
         </div>
-    </div>
+    </header>
     <main role="main">
         <nav class="article-category ps-5 py-2" aria-label="breadcrumb">
             <ol class="breadcrumb my-0">
